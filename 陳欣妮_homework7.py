e68865725408a471650d21225558ab68772602f9
Sunny6115 <71537585+Sunny6115@users.noreply.github.com>
2022-05-14 09:56:39 +0800

    Add files via upload

--- 陳欣妮_homework7.py
+++ 陳欣妮_homework7.py
@@ -5,15 +5,15 @@ chrome = webdriver.Chrome()#使用程控瀏覽器
 chrome.get("https://tw.eztable.com/search?country=tw&date=2022-05-11&people=2&searchTab=restaurant&source=mobile.eztable.com")#至該網站
 time.sleep(3)
 
-for i in range(2):
+for i in range(7):
     chrome.execute_script('window.scrollTo(0,document.body.scrollHeight);')#將拉調往下拉
     time.sleep(1)
     
 soup = BeautifulSoup(chrome.page_source,"html.parser")
-for div in soup.find_all('div',class_ ="row sc-drKuO"):
+for div in soup.find_all('div',class_ ="row sc-drKuOJ cJLQnl"):
     Name = div.find("h4")
     productName = Name.text
-    Price = div.find('div',class_ ="sc-cugefK gFcUdK")
+    Price = div.find('span',class_ ="sc-cugefK gFcUdK")
     productPrice = Price.text
     print('餐廳:'+productName+'價格:'+productPrice)
 chrome.close()
