5af93ee7bd45d76ae5239f13c58b9f099684d1ec
Sunny6115 <71537585+Sunny6115@users.noreply.github.com>
2022-05-15 21:20:59 +0800

    Add files via upload

---
+++ 陳欣妮_Homework8-1.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+chrome = webdriver.Chrome()#使用程控瀏覽器
+chrome.get("https://www.gmail.com")#至該網站
+time.sleep(0.5)
+inputBar = chrome.find_element_by_tag_name("Xb9hP")
+inputBar.send_keys("player333617@gmail.com")
+time.sleep(0.5)
+inputBar.send_keys(Keys.ENTER)
+
+"""
+time.sleep(0.5)
+chrome.find_element_by_partial_link_text("猿創力程式設計學校-")
+time.sleep(0.5)
+chrome.maximize_window()
+time.sleep(0.5)
+chrome.get_screenshot_as_file("猿創力.png")
+time.sleep(0.5)
+"""
+chrome.close()
---
+++ 陳欣妮_Homework8-2.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+chrome = webdriver.Chrome()
+chrome.get("https://pic.sogou.com/")
+time.sleep(0.5)
+search = input("請問您想查詢什麼圖片呢?")
+#inputBar = chrome.find_element_by_id("form_querytext")
+def GoogleSearch(search):
+    inputBar = chrome.find_element_by_name("query")
+    inputBar.send_keys(search)
+    inputBar.send_keys(Keys.ENTER)
+    time.sleep(0.5)
+    chrome.maximize_window()
+    time.sleep(0.5)
+    for i in range(2):
+        chrome.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+        time.sleep(2)
+    time.sleep(2)
+    i=1
+    #for element in chrome.find_elements_by_css_selector('img.img-hover'):
+    #for element in chrome.find_elements_by_tag_name('img'):
+    for element in chrome.find_elements_by_class_name('img-layout'):
+        img_tag = element.find_element_by_tag_name('img')
+        if img_tag:
+            img_url = img_tag.get_attribute('src')
+            imgRespond = requests.get(img_url)
+            with open("image\\"+'puppy'+str(i)+".jpg","bw") as file:
+                file.write(imgRespond.content)
+        if i ==30:
+            break
+        i+=1
+    time.sleep(0.5)
+chrome.close()
